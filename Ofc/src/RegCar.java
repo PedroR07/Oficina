1e692c24066bdc96dd9f924e2323894c1c4b5124
ParanoidAndroid <136632613+PedroR07@users.noreply.github.com>
2023-11-09 10:47:37 -0300

    Add files via upload

---
+++ Ofc/src/RegCar.java
@@ -0,0 +1,13 @@
+
+public class RegCar {
+    String nomeCarro;
+    String marca;
+    int ano;
+    int tipoServiço;
+    //1: Alinhamento;
+    //2: Balanceamento;
+    //3: Revisão Geral.
+    
+    String necessidadeRequerimento;
+}
+
